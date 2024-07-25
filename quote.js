5285d6252f8f8661102791200c9e8b721b288493
Marco Di Summa <mdisumma@yahoo.com>
2024-07-25 17:43:43 +0100

    appendFile

--- quote.js
+++ quote.js
@@ -3,7 +3,10 @@ import { stringify } from "node:querystring";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
-const text = `{ "name": "marco" }`;
+
+function textIn(text) {
+  return `{"id":"${uuidv4()}","quoteText": "${text}"}`;
+}
 
 export async function getQuotes(filePath) {
   try {
@@ -13,18 +16,27 @@ export async function getQuotes(filePath) {
     console.error("Error reading file:", error);
   }
 }
-// const result = await getQuotes(fileName);
-// console.log(result);
 
-export async function addQuote(filePath, data) {
+export async function addQuote(filePath, quote) {
   try {
-    await writeFile(filePath, data, "utf8");
+    await writeFile(filePath, quote, "utf8");
     console.log("File written successfully");
   } catch (error) {
     console.error("Error writing file:", error);
   }
 }
-await addQuote(fileName, text);
+
+export async function appendQuote(filePath, quote) {
+  try {
+    await appendFile(filePath, quote, "utf8");
+    console.log("File appended successfully");
+  } catch (error) {
+    console.error("Error writing file:", error);
+  }
+}
+
+// await addQuote(fileName, textIn("some text"));
+await appendQuote(fileName, textIn("some text"));
 
 const result = await getQuotes(fileName);
 console.log(result);

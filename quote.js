b94515dfd8901ee47ed6773c8abb2a9d27fffa08
Marco Di Summa <mdisumma@yahoo.com>
2024-07-25 16:42:15 +0100

    tiket 1 - 2?

--- quote.js
+++ quote.js
@@ -1,24 +1,36 @@
-import fs from "node:fs/promises";
+import fs, { readFile, writeFile, appendFile } from "node:fs/promises";
+import { stringify } from "node:querystring";
 import { v4 as uuidv4 } from "uuid";
 
 const fileName = "quotes.json";
+const text = `{ "name": "marco" }`;
 
-export async function getQuotes() {
-
+export async function getQuotes(filePath) {
+  try {
+    const data = await readFile(filePath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
 }
+// const result = await getQuotes(fileName);
+// console.log(result);
 
-export async function addQuote(quoteText) {
-
+export async function addQuote(filePath, data) {
+  try {
+    await writeFile(filePath, data, "utf8");
+    console.log("File written successfully");
+  } catch (error) {
+    console.error("Error writing file:", error);
+  }
 }
+await addQuote(fileName, text);
 
-export async function getRandomQuote() {
+const result = await getQuotes(fileName);
+console.log(result);
 
-}
+// export async function getRandomQuote() {}
 
-export async function editQuote(id, quoteText) {
+// export async function editQuote(id, quoteText) {}
 
-}
-
-export async function deleteQuote(id) {
-
-}
+// export async function deleteQuote(id) {}
